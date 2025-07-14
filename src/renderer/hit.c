d8bce0ee0df6980ee9973f98ff45965242003557
Hakob Aghajanyan <haaghaja@c1r5s3.42yerevan.am>
2025-07-14 19:53:50 +0400

    updated maps

--- src/renderer/hit.c
+++ src/renderer/hit.c
@@ -6,7 +6,7 @@
 /*   By: 032zolotarev <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 10:52:17 by 032zolotarev      #+#    #+#             */
-/*   Updated: 2025/07/14 16:17:11 by azolotar         ###   ########.fr       */
+/*   Updated: 2025/07/14 19:53:06 by haaghaja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 #include "utils.h"
 #include "renderer.h"
 #include "defines.h"
+
+/*
+bool is_hitable(t_vec3 u, t_vec3 p1, t_obj *obj)
+{
+    t_vec3 q  = obj->center;
+
+    t_vec3 v = v_sub(p1, q);
+
+    t_vec3 cross = v_cross(u, v);
+    float dist = v_len(cross) / v_len(u);
+
+    return (dist <= obj->attrs[0]); // radius
+}
+*/
 
 /*
  * need to add normal for cylinder - done
@@ -30,10 +44,15 @@ bool	find_hit(t_ray *ray, t_rt *info, t_hit *hit, bool shadow_hit)
 	bool	tmp_reverse = false;
 
     i = -1;
+    //t_vec3 p1 = ray->origin;
+    //t_vec3 p2 = v_add(ray->origin, ray->direction);
+    //t_vec3 u = v_sub(p2, p1);
     while (++i < info->scene->objs_count)
     {
         t = -1.0f;
         obj = &info->scene->objs[i];
+		//if (obj->type == SPHERE && !is_hitable(u, p1, obj))
+		//	continue ;
         if (obj->type == PLANE)
             t = intersect_plane(ray, obj, &tmp_reverse);
         else if (obj->type == SPHERE)
