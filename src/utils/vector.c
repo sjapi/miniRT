2e0768c53fdfbbababe16eaf696ee774c1ce49f8
sjapi <aguznenkovv@yandex.ru>
2025-07-16 19:42:37 +0400

    Add solar scene

--- src/utils/vector.c
+++ src/utils/vector.c
@@ -6,7 +6,7 @@
 /*   By: 032zolotarev <marvin@42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 09:45:42 by 032zolotarev      #+#    #+#             */
-/*   Updated: 2025/07/09 12:24:55 by 032zolotarev     ###   ########.fr       */
+/*   Updated: 2025/07/17 16:50:48 by azolotar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,14 @@ static bool	in_range(float val, float min, float max)
 	return (val >= min && val <= max);
 }
 
-bool	v_normalized(t_vec3 p)
-{
-	return (
-		in_range(p.x, -1.0, 1.0)
-		&& in_range(p.y, -1.0, 1.0)
-		&& in_range(p.z, -1.0, 1.0)
-	);
-}
-
 float	v_len(t_vec3 v)
 {
 	return (sqrtf(v.x * v.x + v.y * v.y + v.z * v.z));
+}
+
+bool	v_normalized(t_vec3 p)
+{
+	return (fabsf(v_len(p) - 1.0) <= 0.001);
 }
 
 t_vec3	v_add(t_vec3 a, t_vec3 b)
